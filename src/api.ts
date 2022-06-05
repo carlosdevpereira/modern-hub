edc118483f80d1610d49e94f585a8bea726a03d3
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-06-05 22:27:12 +0100

    test: github api class

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,24 @@
 import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
 import type { Octokit as OctokitInstance } from 'octokit'
 
-export default class $Github {
+export interface Api {
+	instance: OctokitInstance
+	rest: OctokitInstance['rest']
+	request: OctokitInstance['request']
+}
+
+export default class $Github implements Api {
+	instance
+
 	rest
 
 	request
 
 	constructor() {
 		const accessToken = localStorage.getItem('_access_token_')
-		const octokit = new Octokit({ auth: accessToken }) as OctokitInstance
+		this.instance = new Octokit({ auth: accessToken }) as OctokitInstance
 
-		this.rest = octokit.rest
-		this.request = octokit.request
+		this.rest = this.instance.rest
+		this.request = this.instance.request
 	}
 }
