c0df8d87e331d859596a3ab119bb2f3e1cc529a2
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-06-05 21:46:15 +0100

    feat: generate code coverage reports and upload to codecov (#28)

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
-import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
+	},
+	test: {
+		environment: 'jsdom',
+		coverage: {
+			reporter: ['text-summary', 'clover', 'lcov'],
+			all: true,
+			include: ['src/**'],
+			exclude: ["src/typings/**", "dist/**", "coverage/**", "packages/*/test{,s}/**", "**/*.d.ts", "test{,s}/**", "test{,-*}.{js, cjs, mjs, ts, tsx, jsx}", "**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}", "**/__tests__ /** ", " ** /{ava,babel,nyc}.config.{js,cjs,mjs}", "**/jest.config.{ js, cjs, mjs, ts }", "**/{karma,rollup,webpack}.config.js", "**/.{eslint,mocha}rc.{js,cjs}"]
+		}
 	},
 	plugins: [
 		Vue({
